378486b37cf7e06690838b9d11231bd624caa052
transfarmer <username11681@gmail.com>
2020-05-30 05:01:42 +0100

    update dependencies

--- src/main/java/user11681/limitless/Main.java
+++ src/main/java/user11681/limitless/Main.java
@@ -4,13 +4,13 @@ import java.util.Collection;
 import java.util.Collections;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import user11681.anvil.entrypoint.ListenerInitializer;
+import user11681.anvil.entrypoint.CommonListenerInitializer;
 import user11681.anvil.event.Listener;
 import user11681.anvilevents.event.i18n.TranslationEvent;
 import user11681.usersmanual.math.RomanNumerals;
 
-public class Main implements ListenerInitializer {
-    public static final String MOD_ID = "limitless";;
+public class Main implements CommonListenerInitializer {
+    public static final String MOD_ID = "limitless";
 
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
