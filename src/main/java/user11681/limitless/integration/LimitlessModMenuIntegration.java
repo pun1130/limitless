65fb3589bdf2130fc8be084f852e0a9d5d356ef5
user11681 <username11681@gmail.com>
2021-07-06 18:16:32 +0100

    bump patch

--- src/main/java/user11681/limitless/integration/LimitlessModMenuIntegration.java
+++ src/main/java/user11681/limitless/integration/LimitlessModMenuIntegration.java
@@ -3,12 +3,11 @@ package user11681.limitless.integration;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.client.gui.screen.Screen;
 import user11681.limitless.config.LimitlessConfiguration;
 
 public class LimitlessModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (Screen parent) -> AutoConfig.getConfigScreen(LimitlessConfiguration.class, parent).get();
+        return parent -> AutoConfig.getConfigScreen(LimitlessConfiguration.class, parent).get();
     }
 }
