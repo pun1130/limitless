79946272f41603d9118df43291072b91f3b65707
user11681 <username11681@gmail.com>
2021-07-06 18:15:48 +0100

    use Mod Menu from CF; use new Mod Menu package

--- src/main/java/user11681/limitless/integration/LimitlessModMenuIntegration.java
+++ src/main/java/user11681/limitless/integration/LimitlessModMenuIntegration.java
@@ -1,8 +1,7 @@
 package user11681.limitless.integration;
 
-/*
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.screen.Screen;
 import user11681.limitless.config.LimitlessConfiguration;
@@ -13,4 +12,3 @@ public class LimitlessModMenuIntegration implements ModMenuApi {
         return (Screen parent) -> AutoConfig.getConfigScreen(LimitlessConfiguration.class, parent).get();
     }
 }
-*/
