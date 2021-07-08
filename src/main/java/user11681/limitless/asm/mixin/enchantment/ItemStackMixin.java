16a9213bed988ae54087fe7f250b8be7b70170e7
user11681 <username11681@gmail.com>
2021-07-08 23:01:48 +0100

    disable a narrowing enchantment level conversion to byte in ItemStack#addEnchantment

--- src/main/java/user11681/limitless/asm/mixin/enchantment/ItemStackMixin.java
+++ src/main/java/user11681/limitless/asm/mixin/enchantment/ItemStackMixin.java
@@ -1,6 +1,9 @@
 package user11681.limitless.asm.mixin.enchantment;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,5 +16,10 @@ abstract class ItemStackMixin {
                        target = "Lnet/minecraft/item/ItemStack;hasEnchantments()Z"))
     public boolean makeReenchantable(ItemStack stack) {
         return !LimitlessConfiguration.instance.enchantment.reenchanting.allowEquipment() && stack.hasEnchantments();
+    }
+
+    @Redirect(method = "addEnchantment", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;createNbt(Lnet/minecraft/util/Identifier;I)Lnet/minecraft/nbt/NbtCompound;"))
+    public NbtCompound disableNarrowingConversion(Identifier id, int level) {
+        return EnchantmentHelper.createNbt(id, level);
     }
 }
